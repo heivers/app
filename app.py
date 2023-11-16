391766e95c7b8a0b932d6e3c1568a2b8a733c8a6
Bart <bart.heerkens@gmail.com>
2023-11-16 12:22:23 +0100

    commit from Thu 16 Nov 12:22:23 CET 2023

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ from datetime import datetime, timedelta
 import altair as alt
 
 data = pd.read_pickle("data/dataframe.pkl")
-#vie = vie.set_index("date")
-#md = pd.read_pickle("data/moedling.pkl")
-#md = md.set_index("date")
 
 st.markdown("# Bart's :penguin: data :rainbow[assignment]!")
 
 st.markdown("### A short :blue[summary:]")
-st.markdown("""This project automatically downloads air quality, :thermometer: and humidty data from Vienna and Mödling.\
+st.markdown("""This project (my not exactely :strawberry: Pi) automatically downloads air quality, :thermometer: and humidty data from Vienna and Mödling.\
             This data is then converted into 2 csv files with automated Bash/awk scripts,
             cleaned up a little more with Python and uploaded to this Streamlit app.
             Mödling (my home) is just outside of Vienna. Does it have better air-quality?""")
@@ -33,40 +30,22 @@ with st.sidebar:
     "Select the item you wish to view",
     data.columns
     )
-#start_date = min_date #remove later
+
 end_date = start_date + timedelta(days=1)
-
-
-#moed_temp = md.loc[start_date:end_date][[option]]
-#wien_temp = vie.loc[start_date:end_date][[option]]
-
-#moed_temp["moedling"] = moed_temp[option]
-#wien_temp["wien"] = wien_temp[option]
-
-# chart1 = alt.Chart(moed_temp.reset_index()).mark_line().encode(
-#     x= alt.X("date:T", title="date"),
-#     y= alt.Y("moedling", title=option),
-#     color=alt.value("blue"),
-# )
-
-# chart2 = alt.Chart(wien_temp.reset_index()).mark_line().encode(
-#     x= alt.X("date:T", title="date"),
-#     y= alt.Y("wien", title=option),
-#     color=alt.value("red"),
-#     text = alt.Text(alt.value("Wien"))
-# )
-
-# chart1 + chart2
 
 data_temp = data[option].unstack(level=0)
 data_temp = data_temp.loc[start_date:end_date]
+data_temp = data_temp[:-1]
 
 st.subheader(f"{option.title()}: Mödling <-> Wien")
 st.line_chart(data_temp, y=["moedling", "vienna"])
-# st.subheader("Vienna")
-# st.line_chart(wien_temp, y=[option, option2])
 
+st.divider()
 
+st.caption(f"The actual data for {option.title()}")
+st.dataframe(data_temp)
 
+st.divider()
 
-
+st.text("Please note that the analytics was not the main part of this assignment.")
+st.text("Some values were missing and were backfilled.")
